c1e94968e6205880dcf352de3a7843b39f04143d
Eric <eric.himmelbauer@protonmail.com>
2021-02-17 19:04:48 +0100

    added calendar

--- src/web/routes/frontpage.js
+++ src/web/routes/frontpage.js
@@ -1,4 +1,5 @@
 "use strict";
+const {Sequelize, Op} = require("sequelize");
 const {Article} = require("../../model/article");
 
 /**
@@ -11,7 +12,6 @@ const {Article} = require("../../model/article");
  * @param {Request} req
  * @param {Response} res
  * @param {NextFunction} next
- * @param {ServiceCollection} serviceCollection
  */
 module.exports = async(req, res, next) => {
     const articles = await Article.findAll({
@@ -19,9 +19,40 @@ module.exports = async(req, res, next) => {
         limit: 10
     });
 
+    const startDate = new Date(new Date().setDate(1));
+    let articleCount = {};
+    let relevantArticles = await Article.findAll({
+        attributes: ["createdAt"],
+        raw: true
+    });
+    for(let i = 0; i < 7*6; i++) {
+        let date = new Date(startDate.valueOf());
+        date.setDate(date.getDate() + i);
+        date = new Date(date.valueOf());
+        articleCount[date.toDateString()] = relevantArticles.filter(x => x.createdAt.toDateString() === date.toDateString()).length;
+    }
+
     res.render("frontpage", {
         user: req.blog?.user,
         nav: req.nav,
-        articles
+        articles,
+        startDate,
+        /**
+         * @param {Date} startDate
+         * @param {Date} date
+         * @param {Number} index
+         * @returns {{class: Array<String>, href: String}}
+         */
+        dateFormatter: (startDate, date, index) => {
+            let href = articleCount[date.toDateString()] > 0 ? `/articles/index/date/${date.toISOString().substring(0, 10)}` : "";
+            let classPrefix = href ? "link" : "text";
+            return {
+                class: [
+                    new Date().toDateString() === date.toDateString() ? `${classPrefix}-success` : "",
+                    date?.getMonth() === startDate?.getMonth() ? `${classPrefix}-primary` : `${classPrefix}-secondary`
+                ],
+                href: href
+            };
+        }
     });
 };
--- src/web/routes/index_article.js
+++ src/web/routes/index_article.js
@@ -1,17 +1,32 @@
 "use strict";
+const { Op } =  require("sequelize");
 const { Article } = require("../../model/article");
 const { Tag } = require("../../model/tag");
 
 /**
- * @typedef {import("express").Request} Request
- * @typedef {import("express").Response} Response
- * @typedef {import("express").NextFunction} NextFunction
+ * 
+ * @param {import("express").Request<{date: Date}>} req 
+ * @param {import("express").Response} res 
+ * @param {import("express").NextFunction} next 
  */
-
 module.exports = async(req, res, next) => {
+    let where = {};
+    const {date} = req.params;
+    if(date) {
+        let tomorrow = new Date(date.valueOf());
+        tomorrow.setDate(tomorrow.getDate() + 1);
+        where = {
+            "createdAt": {
+                [Op.gte]: date,
+                [Op.lt]: tomorrow
+            }
+        }
+    }
+
     const articles = await Article.findAll({
         order: [["updatedAt", "DESC"]],
-        include: [Tag]
+        include: [Tag],
+        where: where
     });
 
     res.render("article_index", {
